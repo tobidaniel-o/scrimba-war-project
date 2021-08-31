d8d034d7017e5c00eda2d10d233ee795c14d6539
Tobi Okunuga <tobi.daniel2018@gmail.com>
2021-08-30 23:04:23 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// const newDeck = document.querySelector("#new-deck");
+
+// const handleClick = () =>
+//   fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+//     .then((response) => response.json())
+//     .then((result) => console.log(result));
+
+// const callback = () => {
+//   console.log("I finally ran!");
+// };
+
+// setTimeout(callback, 2000);
+
+// newDeck.addEventListener("click", () => handleClick());
+
+const people = [
+  { name: "Jack", hasPet: true },
+  { name: "Jill", hasPet: false },
+  { name: "Alice", hasPet: true },
+  { name: "Bob", hasPet: false },
+];
+
+function filterArray(array, callback) {
+  const resultingArray = []
+}
+
+// function gimmeThePets(person) {
+//   return person.hasPet;
+// }
+
+// const peopleWithPets = people.filter(gimmeThePets);
+
+// console.log(peopleWithPets);
