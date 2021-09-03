07b91f9a85e630540f67cc2714e2aa4e6e30354b
Tobi Okunuga <tobi.daniel2018@gmail.com>
2021-09-02 22:05:54 -0400

    added the feature

---
+++ commentedCode.js
@@ -0,0 +1,74 @@
+/**
+ * Challenge: method chaining!
+ *
+ * 1. Select the button in the DOM and add an event listener to it without saving the DOM element as a separate variable. I.e. "chain" the `addEventListener` on after your `getElementById()`(When clicked, log "Clicked" to the console)
+ *    - I realize this might feel like busywork, but my intent will make sense soon
+ *
+ * 2. Given the array below, chain the `.filter` and `.map` array methods together to turn the array into an array of string email addresses of only the people in the array who voted. Log the array of email addresses to the console
+ */
+
+//
+
+// const voters = [
+//   { name: "Joe", email: "joe@joe.com", voted: true },
+//   { name: "Jane", email: "jane@jane.com", voted: true },
+//   { name: "Bo", email: "bo@bo.com", voted: false },
+//   { name: "Bane", email: "bane@bane.com", voted: false },
+// ];
+
+// // Write your code below
+// const finalResult = voters
+//   .filter((voter) => voter.voted)
+//   .map((voter) => voter.email);
+
+// document
+//   .getElementById("new-deck")
+//   .addEventListener("click", () => console.log(finalResult));
+
+// Final result: ["joe@joe.com", "jane@jane.com"]
+
+// ###############
+// // const newDeck = document.querySelector("#new-deck");
+
+// // const handleClick = () =>
+// //   fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+// //     .then((response) => response.json())
+// //     .then((result) => console.log(result));
+
+// // const callback = () => {
+// //   console.log("I finally ran!");
+// // };
+
+// // setTimeout(callback, 2000);
+
+// // newDeck.addEventListener("click", () => handleClick());
+
+// const people = [
+//   { name: "Jack", hasPet: true },
+//   { name: "Jill", hasPet: false },
+//   { name: "Alice", hasPet: true },
+//   { name: "Bob", hasPet: false },
+// ];
+
+// function filterArray(array, callback) {
+//   const resultingArray = [];
+
+//   for (let item of array) {
+//     const shouldBeIncluded = callback(item);
+//     if (shouldBeIncluded) {
+//       resultingArray.push(item);
+//     }
+//   }
+//   return resultingArray;
+// }
+
+// // function gimmeThePets(person) {
+// //   return person.hasPet;
+// // }
+
+// // const peopleWithPets = people.filter(gimmeThePets);
+
+// // console.log(peopleWithPets);
+
+// const peopleWithPets = filterArray(people, (person) => person.hasPet);
+// console.log(peopleWithPets)
--- script.js
+++ script.js
@@ -1,75 +1,74 @@
+let deckId;
+const cardsContainer = document.getElementById("cards");
+const newDeckBtn = document.getElementById("new-deck");
+const drawCardBtn = document.getElementById("draw-cards");
+const header = document.getElementById("header");
+const remainingText = document.getElementById("remaining");
+
+function handleClick() {
+  fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+    .then((res) => res.json())
+    .then((data) => {
+      remainingText.textContent = `Remaining cards: ${data.remaining}`;
+      deckId = data.deck_id;
+      console.log(deckId);
+    });
+}
+
+newDeckBtn.addEventListener("click", handleClick);
+
 /**
- * Challenge: method chaining!
+ * Challenge:
  *
- * 1. Select the button in the DOM and add an event listener to it without saving the DOM element as a separate variable. I.e. "chain" the `addEventListener` on after your `getElementById()`(When clicked, log "Clicked" to the console)
- *    - I realize this might feel like busywork, but my intent will make sense soon
- *
- * 2. Given the array below, chain the `.filter` and `.map` array methods together to turn the array into an array of string email addresses of only the people in the array who voted. Log the array of email addresses to the console
+ * Display the number of cards remaining in the deck on the page
+ * Hint: Check the data that comes back when we draw 2 new cards
+ * to see if there's anything helpful there for this task (😉)
  */
 
-// document.getElementById("new-deck").addEventListener("click", function() {
-//     console.log("Clicked!")
-// })
+drawCardBtn.addEventListener("click", () => {
+  fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    .then((res) => res.json())
+    .then((data) => {
+      remainingText.textContent = `Remaining cards: ${data.remaining}`;
+      cardsContainer.children[0].innerHTML = `
+                <img src=${data.cards[0].image} class="card" />
+            `;
+      cardsContainer.children[1].innerHTML = `
+                <img src=${data.cards[1].image} class="card" />
+            `;
+      const winnerText = determineCardWinner(data.cards[0], data.cards[1]);
+      header.textContent = winnerText;
 
-const voters = [
-  { name: "Joe", email: "joe@joe.com", voted: true },
-  { name: "Jane", email: "jane@jane.com", voted: true },
-  { name: "Bo", email: "bo@bo.com", voted: false },
-  { name: "Bane", email: "bane@bane.com", voted: false },
-];
+      if (data.remaining === 0) {
+        drawCardBtn.disabled = true;
+      }
+    });
+});
 
-// Write your code below
-const emailForVotes = voters
-  .filter((voter) => voter.voted)
-  .map((voter) => voter.email);
+function determineCardWinner(card1, card2) {
+  const valueOptions = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "JACK",
+    "QUEEN",
+    "KING",
+    "ACE",
+  ];
+  const card1ValueIndex = valueOptions.indexOf(card1.value);
+  const card2ValueIndex = valueOptions.indexOf(card2.value);
 
-document
-  .getElementById("new-deck")
-  .addEventListener("click", () => console.log(emailForVotes));
-
-// Final result: ["joe@joe.com", "jane@jane.com"]
-
-// // const newDeck = document.querySelector("#new-deck");
-
-// // const handleClick = () =>
-// //   fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
-// //     .then((response) => response.json())
-// //     .then((result) => console.log(result));
-
-// // const callback = () => {
-// //   console.log("I finally ran!");
-// // };
-
-// // setTimeout(callback, 2000);
-
-// // newDeck.addEventListener("click", () => handleClick());
-
-// const people = [
-//   { name: "Jack", hasPet: true },
-//   { name: "Jill", hasPet: false },
-//   { name: "Alice", hasPet: true },
-//   { name: "Bob", hasPet: false },
-// ];
-
-// function filterArray(array, callback) {
-//   const resultingArray = [];
-
-//   for (let item of array) {
-//     const shouldBeIncluded = callback(item);
-//     if (shouldBeIncluded) {
-//       resultingArray.push(item);
-//     }
-//   }
-//   return resultingArray;
-// }
-
-// // function gimmeThePets(person) {
-// //   return person.hasPet;
-// // }
-
-// // const peopleWithPets = people.filter(gimmeThePets);
-
-// // console.log(peopleWithPets);
-
-// const peopleWithPets = filterArray(people, (person) => person.hasPet);
-// console.log(peopleWithPets)
+  if (card1ValueIndex > card2ValueIndex) {
+    return "Card 1 wins!";
+  } else if (card1ValueIndex < card2ValueIndex) {
+    return "Card 2 wins!";
+  } else {
+    return "War!";
+  }
+}
